36326f5f84446c9191756f81d3d367a1c7e16f5d
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2020-02-13 16:08:41 -0500

    29. CreacionViewModelLiveData

---
+++ app/src/main/java/com/platzi/conf/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,34 @@
+package com.platzi.conf.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.platzi.conf.model.Conference
+import com.platzi.conf.network.Callback
+import com.platzi.conf.network.FirestoreService
+import java.lang.Exception
+
+class ScheduleViewModel {
+    val firestoreService = FirestoreService()
+    var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getScheduleFromFirebase()
+    }
+
+    fun getScheduleFromFirebase() {
+        firestoreService.getSchedule(object: Callback<List<Conference>> {
+            override fun onSuccess(result: List<Conference>?) {
+                listSchedule.postValue(result)
+                processFinished()
+            }
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+        })
+    }
+
+    fun processFinished() {
+        isLoading.value = true
+    }
+}
